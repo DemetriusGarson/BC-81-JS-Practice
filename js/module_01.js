6b424053fb5a31758ebacaa69a036f2f87e50263
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-05-21 23:25:49 +0300

    module_1-1

---
+++ js/module_01.js
@@ -0,0 +1,61 @@
+// ! ==============TASK 1
+
+// Створи змінну з твоїм іменем, віком і чи є у тебе домашній улюбленець (true/false). Виведи всі три в консоль.
+
+// const name = "Dmytro";
+
+// const age = 34;
+
+// const pet = false;
+
+// console.log(name, age, pet);
+
+// ! ==============TASK 2
+
+// Створи змінну price зі значенням 150 і змінну discount зі значенням 20. Порахуй фінальну ціну і виведи результат.
+
+// const price = 270;
+
+// const discount = 5;
+
+// const discauntValue = (price * discount) / 100;
+
+// const totalPrice = price - discauntValue;
+
+// console.log(totalPrice);
+
+// ! ==============TASK 3
+
+// Створи змінну з твоїм повним іменем. Виведи кількість символів у ньому.
+
+// const fullName = 'Dmytro Havrysh';
+
+// console.log(fullName.length);
+
+// ! ==============TASK 4
+
+// Є рядок "  Привіт, світ!  ". Видали пробіли на початку і в кінці та виведи результат.
+
+// const stringInput = '  Привіт, світ!  ';
+
+// const stringOutput = stringInput.trim();
+
+// console.log(stringOutput);
+
+// ! ==============TASK 5
+
+// Створи змінну password = "myPass123".Перевір чи містить вона цифру "1" і чи починається з "my".Виведи обидва результати.
+
+// const pasword = 'myPass123';
+
+// const hasOne = pasword.includes('1');
+
+// console.log(hasOne);
+
+// const startsWithMy = pasword.startsWith('my');
+
+// console.log(startsWithMy);
+
+// const endsWith123 = pasword.endsWith('123');
+
+// console.log(endsWith123);

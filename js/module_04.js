96f1c6850046858868026a69f8a5812ee21dc5ff
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-06-04 15:02:25 +0300

    module_4-2

--- js/module_04.js
+++ js/module_04.js
@@ -123,3 +123,130 @@
 // }
 
 // console.log(calcTotalPrice(fruits, 'Банан'));
+
+// ! ==============MODULE_4 TASK_5
+
+//* Створи масив, у який потраплять тільки ті товари, в назві яких є слово "iPhone".
+// const products = [
+//   { name: 'Samsung J5 2017', screen: 5.2, price: 5400, weight: 160 },
+//   { name: 'iPhone X', screen: 5.8, price: 25000, weight: 170 },
+//   { name: 'Xiaomi Mi 4', screen: 5.5, price: 4999, weight: 150 },
+//   { name: 'Nokia 3310 2018', screen: 2.4, price: 999, weight: 60 },
+//   { name: 'iPhone 7', screen: 4.7, price: 9999, weight: 140 },
+// ];
+
+// function productFinder(products, needProduct) {
+//   const findArray = [];
+//   for (const product of products) {
+//     if (product.name.includes(needProduct)) {
+//       findArray.push(product);
+//     }
+//   }
+//   return findArray;
+// }
+
+// console.log(productFinder(products, 'iPhone'));
+
+// ! ==============MODULE_4 TASK_6
+
+//* Кошик: додати товар, порахувати суму, знайти товар
+// Дано
+// const cart = {
+//   owner: 'Anna',
+//   items: [
+//     { name: 'phone', price: 12000, quantity: 1 },
+//     { name: 'case', price: 500, quantity: 2 },
+//   ],
+//   getItems() {
+//     return this.items;
+//   },
+//   addItem(product) {
+//     this.items.push(product);
+//   },
+//   getTotalPrice() {
+//     let totalPrice = 0;
+//     for (const item of this.items) {
+//       totalPrice += item.price * item.quantity;
+//     }
+//     return totalPrice;
+//   },
+//   findItem(productName) {
+//     for (const item of this.items) {
+//       if (item.name === productName) {
+//         return item;
+//       }
+//       return null;
+//     }
+//   },
+// };
+
+// console.log(cart.getTotalPrice());
+
+// console.log(cart.findItem('case'));
+
+// Потрібно
+
+// Додати в об’єкт cart методи:
+
+// getItems()
+// має повертати масив товарів
+
+// addItem(product)
+// має додавати новий товар у items
+
+// getTotalPrice()
+// має повертати загальну суму з урахуванням price * quantity
+
+// findItem(productName)
+// має повертати об’єкт товару з таким ім’ям або null, якщо товар не знайдено
+
+// ! ==============MODULE_4 TASK_7
+
+// Команда розробників: сума зарплат і додавання нового учасника
+// Допиши методи:
+
+// getTotalSalary() має повертати суму зарплат усіх учасників
+// addMember(newMember) має додавати нового учасника в масив members
+// checkUserAge(); коли цей метод выкликается для коджного участника команди додается поле isAdult в залежности вид того чи вказанний його вик(поле age)
+
+// const team = {
+//   members: [
+//     { name: 'Anna', role: 'designer', salary: 1500, age: 25 },
+//     { name: 'John', role: 'developer', salary: 2500, age: 17 },
+//     { name: 'Kate', role: 'manager', salary: 2000 },
+//   ],
+//   getTotalSalary() {
+//     let totalSallary = 0;
+//     for (const member of this.members) {
+//       totalSallary += member.salary;
+//     }
+//     return totalSallary;
+//   },
+//   addMember(newMember) {
+//     this.members.push(newMember);
+//   },
+//   checkUserAge() {
+//     const newArray = [];
+
+//     for (const member of this.members) {
+//       if ('age' in member) {
+//         if (member.age >= 18) {
+//           newArray.push({ ...member, isAdult: true });
+//         } else {
+//           newArray.push({ ...member, isAdult: false });
+//         }
+//       } else {
+//         newArray.push({ ...member });
+//       }
+//     }
+//     return newArray;
+//   },
+// };
+
+// console.log(team.getTotalSalary());
+
+// team.addMember({ name: 'Mike', role: 'designer', salary: 1900, age: 37 });
+
+// console.log(team.members);
+
+// console.log(team.checkUserAge());

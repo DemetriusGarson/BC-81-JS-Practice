9041f37202f4e7b2c44c54d7017fa3a2a63aa3d9
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-06-07 22:39:02 +0300

    theory_module_06

---
+++ js/theory_module_06.js
@@ -0,0 +1,101 @@
+// const objC = { c: 'objC prop' };
+
+// const objB = Object.create(objC);
+// objB.b = 'objB prop';
+
+// const objA = Object.create(objB);
+// objA.a = 'objA prop';
+
+// console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+// console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+// console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+//!
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   access;
+
+//   static role = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   constructor(params) {
+//     super(params.email);
+//     this.access = params.access;
+//   }
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   access: Admin.role.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.access); // "superuser"
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  static role = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+
+  blacklistedEmails = [];
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+
+  constructor(params) {
+    super(params.email);
+    this.access = params.access;
+  }
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted('mango@mail.com')); // false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
